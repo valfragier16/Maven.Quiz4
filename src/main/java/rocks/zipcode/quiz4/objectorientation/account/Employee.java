5d6f54c22b9b0adc391a88e06f272bb8639cf42b
val <valfragier@gmail.com>
2019-12-09 20:12:36 -0500

    Almost Done

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -3,14 +3,24 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 30/12/2018.
  */
-public class Employee implements Worker{
+public class Employee implements Worker, Transactable{
     BankAccount bankAccount;
+    Double hoursWorked;
+    Double hourlyWage = 35.0;
+    Double moneyEarned;
 
     public Employee() {
+
+        this.bankAccount = new BankAccount();
+        this.hoursWorked = 0.0;
+        this.moneyEarned = 0.0;
     }
 
     public Employee(BankAccount bankAccount) {
+
         this.bankAccount = bankAccount;
+        this.hoursWorked = 0.0;
+        this.moneyEarned = 0.0;
     }
 
     public BankAccount getBankAccount() {
@@ -24,20 +34,49 @@ public class Employee implements Worker{
 
     @Override
     public void increaseHoursWorked(Double numberOfHours) {
+        this.hoursWorked = hoursWorked + numberOfHours;
     }
 
     @Override
     public Double getHoursWorked() {
-        return null;
+        return this.hoursWorked;
     }
 
     @Override
     public Double getHourlyWage() {
-        return null;
+        return this.hourlyWage;
     }
 
     @Override
     public Double getMoneyEarned() {
-        return null;
+        return this.moneyEarned;
     }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+        if(amountToIncreaseBy <= 0){
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.bankAccount.deposit(amountToIncreaseBy);
+        }
+
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        if(amountToDecreaseBy < 0){
+            throw new IllegalArgumentException();
+        }
+        else {
+            this.bankAccount.withdrawal(amountToDecreaseBy);
+        }
+    }
+
+    @Override
+    public Double getBalance() {
+        return bankAccount.getBalance();
+    }
+
+
 }
